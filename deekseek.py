999bc812054e8c6a003bcb1e2746e636108f103e
Pavel Bakunovich <online@bakunovich.me>
2025-03-24 17:16:09 +0300

    DeepSeek client added

---
+++ deekseek.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import constants
+
+load_dotenv()
+DEEPSEEK_API_KEY = os.environ['DEEPSEEK_API_KEY']
+
+def send_request_deekseek(prompt):
+    client = OpenAI(api_key=DEEPSEEK_API_KEY, base_url="https://api.deepseek.com")
+
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {"role": "system", "content": constants.SYSTEM_PROMPT_DEEPSEEK},
+            {"role": "user", "content": prompt},
+        ],
+        temperature=1.5,
+        stream=False
+    )
+
+    return response.choices[0].message.content
+
+print(send_request_deekseek("Расскажи очень обидную шутку про Максима Окунева."))
+
