e8ed87d6e29b7ab5f1f56a3bd856c1c54d94a7ec
Pavel Bakunovich <online@bakunovich.me>
2025-03-25 22:20:12 +0300

    Changed date formatting in logger

--- logger.py
+++ logger.py
@@ -1,12 +1,14 @@
 import helpers
+import datetime
 template_info = "{time}: {message}"
 template_error = "{time}: ERROR: {message}"
+date_format="%d %b %Y %H:%M:%S"
 
 def log(message):
-    print(helpers.fill_template(template_info,time=helpers.get_today_minsk_time(),message=str(message)))
+    print(helpers.fill_template(template_info,time=helpers.get_today_minsk_time().strftime(date_format),message=str(message)))
 
 def log_user_action(user_message_text):
-    log(helpers.fill_template(template_info,time=helpers.get_today_minsk_time(),message=user_message_text))
+    log(helpers.fill_template(template_info,time=helpers.get_today_minsk_time().strftime(date_format),message=user_message_text))
 
 def log_error(error_message):
-    log(helpers.fill_template(template_error,time=helpers.get_today_minsk_time(),message=error_message))
+    log(helpers.fill_template(template_error,time=helpers.get_today_minsk_time().strftime(date_format),message=error_message))
