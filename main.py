2f8fc112e5a239781f301ef9bf43ab3d8253aafb
Pavel Bakunovich <online@bakunovich.me>
2025-03-26 19:32:35 +0300

    Added "Talk" command

--- main.py
+++ main.py
@@ -17,12 +17,6 @@ log("Bot object initialized.")
 
 @bot.message_handler(commands=['add'])
 def add(message):
-    # Split message text into parts and remove command
-    #parts = message.text.split(' ', 1)
-    # Check if parameters were provided
-    #if len(parts) > 1:
-    #    params = parts[1]  # This contains "Sergey Lisovskiy"
-    #    print("Parameters:", params)
     #pin message
     #tb = telebot.TeleBot(api_bot)
     #message = tb.send_message(group_id, 'Test!')
@@ -278,6 +272,37 @@ def joke(message):
 
             bot.reply_to(message, joke)
             log(helpers.fill_template("Joke requested by {name}",name=get_player_name_formal(player)))
+        else:
+            reply_to_unauthorized(bot, message, player)
+    except Exception as e:
+        bot.reply_to(message, constants.UNHANDLED_EXCEPTION_MESSAGE)
+        log_error(e)
+
+@bot.message_handler(commands=['talk'])
+def talk(message):
+    try:
+        player = add_player_if_not_existant(message.from_user.first_name,
+                                            message.from_user.last_name,
+                                            message.from_user.username,
+                                            message.from_user.id)
+        
+        if (helpers.authorized(message.chat.id)):
+            with open(constants.TALK_PROMPT_TEMPLATE_FILENAME,"r") as talk_prompt_template_file:
+                talk_prompt_template_text = talk_prompt_template_file.read()
+           
+            parts = message.text.split(' ', 1)
+            if len(parts) > 1:
+                params = parts[1]
+                talk_prompt_template_text = helpers.fill_template(talk_prompt_template_text, player_prompt=params)
+            else:
+                talk_prompt_template_text = helpers.fill_template(talk_prompt_template_text, player_prompt="Ничего не сказал.")
+
+            talk_prompt_template_text = helpers.fill_template(talk_prompt_template_text, player_name=get_player_name_formal(player))
+
+            response = deepseek.send_request(talk_prompt_template_text, 1.5)
+
+            bot.reply_to(message, response)
+            log(helpers.fill_template("Talk command requested by {name}: \'{player_message}\'",name=get_player_name_formal(player),player_message=params))
         else:
             reply_to_unauthorized(bot, message, player)
     except Exception as e:
