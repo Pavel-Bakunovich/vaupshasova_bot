f7ff64bed17b1c229f997485daae30c20b39d17e
Pavel Bakunovich <online@bakunovich.me>
2025-04-03 20:46:09 +0300

    Removed /joke command

--- constants.py
+++ constants.py
@@ -8,7 +8,6 @@ VAUPSHASOVA_LEAGUE_TELEGRAM_ID=-1001369044567
 UNHANDLED_EXCEPTION_MESSAGE="Чота я паламался. Давай по-новой."
 SQUAD_TEMPLATE_FILENAME = "squad.txt"
 SPLIT_SQUAD_TEMPLATE_FILENAME = "prompt_split_squad.txt"
-JOKE_PROMPT_TEMPLATE_FILENAME = "prompt_joke.txt"
 TALK_PROMPT_TEMPLATE_FILENAME = "prompt_talk.txt"
 SYSTEM_PROMPT_DEEPSEEK = "Ты - полезный ассистент чата в Телеграме, где друзья-футболисты общаются и собирают состав на игру. Этот запрос выполняется в коде Телеграм бота. Будь вредным, саркастичным. Постоянно подшучивай. Подкалывай, но не обидно."
 ABUSIVE_COMMENT_DEEPSEEK = "Вот сообщение от телеграмм бота. Напиши к этому сообщению твой комментарий. Этот комментарий должен звучать прикольно и саркастично по отношению к игроку. \'{bot_message}\'"
