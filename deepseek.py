9433722d239f726594f4bc91140ac8f67fb09925
Pavel Bakunovich <Pavel-Bakunovich@users.noreply.github.com>
2025-04-24 19:13:23 +0000

    Made a change to store context of the conversation with GenAI.

--- deepseek.py
+++ deepseek.py
@@ -6,27 +6,22 @@ import constants
 load_dotenv()
 DEEPSEEK_API_KEY = os.environ['DEEPSEEK_API_KEY']
 
-conversation_id = None
+with open(constants.SYSTEM_PROMPT_FILENAME,"r") as system_prompt_file:
+            system_prompt = system_prompt_file.read()
+
+messages = [{"role": "system", "content": system_prompt}]
 
 def send_request(prompt, temperature):
     client = OpenAI(api_key=DEEPSEEK_API_KEY, base_url="https://api.deepseek.com")
 
-    with open(constants.SYSTEM_PROMPT_FILENAME,"r") as system_prompt_file:
-            system_prompt = system_prompt_file.read()
-
+    messages.append({"role": "user", "content": prompt})
     response = client.chat.completions.create(
         model="deepseek-chat",
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": prompt},
-        ],
+        messages=messages,
         temperature=temperature,
-        stream=False,
-        conversation_id = conversation_id
+        stream=False
     )
 
-    if conversation_id == None:
-        conversation_id = response.get("conversation_id")
-        print(conversation_id)
+    messages.append(response.choices[0].message)
 
     return response.choices[0].message.content
