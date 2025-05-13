328dd30cdeebb87a1c06f490634745a4bdfa57a3
Pavel Bakunovich <Pavel-Bakunovich@users.noreply.github.com>
2025-05-13 08:00:01 +0000

    Added Games table to Db scripts

--- db_create_scripts.sql
+++ db_create_scripts.sql
@@ -6,18 +6,20 @@ CREATE TABLE Players (
     Telegram_ID BIGINT,
     Friendly_First_Name TEXT,
     Friendly_Last_Name TEXT,
-    Informal_Friendly_First_Name TEXT
+    Informal_Friendly_First_Name TEXT,
+    Birthday DATE,
+    Height SMALLINT
 );
 
 CREATE TABLE Matchday (
-   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-   Matchday_Date DATE,
-   Type TEXT,
-   Player_ID INTEGER,
-   Time_Stamp TIMESTAMP WITH TIME ZONE,
-   Wokeup BOOLEAN DEFAULT 'FALSE'
-   CONSTRAINT constraint_1 FOREIGN KEY (Player_ID) REFERENCES public.Players (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
-)
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    Matchday_Date DATE,
+    Type TEXT,
+    Player_ID INTEGER,
+    Time_Stamp TIMESTAMP WITH TIME ZONE,
+    Wokeup BOOLEAN DEFAULT FALSE,
+    CONSTRAINT constraint_1 FOREIGN KEY(Player_ID) REFERENCES public.Players (id) ON UPDATE NO ACTION ON DELETE NO ACTION
+    );
 
 CREATE TABLE Games (
   Game_ID integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
