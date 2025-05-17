f1e9504380f53b0c72f4a244817ee8c6cfe747e9
Pavel Bakunovich <online@bakunovich.me>
2025-05-17 17:11:19 +0300

    Create placeholder for /score

---
+++ command_score.py
@@ -0,0 +1,42 @@
+from logger import log, log_error
+from telebot.types import ReactionTypeEmoji
+from helpers import get_arguments, get_next_matchday, get_next_matchday_formatted, get_today_minsk_time
+from common import add_player_if_not_existant, validate_access_no_game_registration_needed, text_to_image, get_player_name_extended, reply_only_CEO_can_do_it, validate_CEO_zone
+import database
+import constants
+import prettytable as pt
+
+def execute(message, bot):
+    try:
+        current_player = add_player_if_not_existant(message.from_user.first_name,
+                                            message.from_user.last_name,
+                                            message.from_user.username,
+                                            message.from_user.id)
+        bot.reply_to(message, "🛑 Пока эта команда не работает. Не дури галавы.")
+        '''if validate_access_no_game_registration_needed(message.chat.id, current_player, bot, message):
+            table = pt.PrettyTable(['N','Игрок', 'Игры', 'Голы', 'Асисты', 'Автоголы'])
+            table.align['N'] = 'c'
+            table.align['Игрок'] = 'l'
+            table.align['Игры'] = 'c'
+            table.align['Голы'] = 'c'
+            table.align['Асисты'] = 'c'
+            table.align['Автоголы'] = 'c'
+            table.hrules = True
+            season_stats = database.get_season_stats(get_today_minsk_time().year)
+            i = 1
+            
+            for player in season_stats:
+                first_name = player[0]
+                last_name = player[1]
+                games_played = player[2]
+                goals = player[3]
+                assists = player[4]
+                own_goals = player[5]
+                table.add_row([i, f"{first_name} {last_name}", games_played, goals, assists, own_goals])
+                i+=1
+            photo = text_to_image(table.get_string(),image_size=(600, 1000))
+            bot.send_photo(message.chat.id, photo, reply_to_message_id=message.message_id)'''
+    except Exception as e:
+        bot.reply_to(message, constants.UNHANDLED_EXCEPTION_MESSAGE)
+        log_error(e)
+        
--- main.py
+++ main.py
@@ -19,6 +19,7 @@ import command_register_game_stats
 import command_register_money
 import command_register_pitch_payment
 import command_register_score
+import command_score
 import alerts
 import dotenv
 from logger import log
@@ -53,46 +54,37 @@ def vaupshasova_bot(request):
 def add(message):
     command_add.execute(message, bot)
 
-
 @bot.message_handler(commands=['remove'])
 def remove(message):
     command_remove.execute(message, bot)
-
 
 @bot.message_handler(commands=['chair'])
 def chair(message):
     command_chair.execute(message, bot)
 
-
 @bot.message_handler(commands=['squad'])
 def squad(message):
     command_squad.execute(message, bot)
-
 
 @bot.message_handler(commands=['split'])
 def split(message):
     command_split.execute(message, bot)
 
-
 @bot.message_handler(commands=['talk'])
 def talk(message):
     command_talk.execute(message, bot)
-
 
 @bot.message_handler(commands=['wakeup'])
 def wakeup(message):
     command_wakeup.execute(message, bot)
 
-
 @bot.message_handler(commands=['register_lineups'])
 def register_lineups(message):
     command_register_lineups.execute(message, bot)
 
-
 @bot.message_handler(commands=['print_lineups'])
 def print_lineups(message):
     command_print_lineups.execute(message, bot)
-
 
 @bot.message_handler(commands=['season_stats'])
 def season_stats(message):
@@ -133,6 +125,10 @@ def register_pitch_payment(message):
 @bot.message_handler(commands=['register_score'])
 def register_score(message):
     command_register_score.execute(message, bot)
+
+@bot.message_handler(commands=['score'])
+def score(message):
+    command_score.execute(message, bot)
 
 alerts.schedule_alerts()
 log("Alerts scheduled.")
