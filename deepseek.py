6153d4d0f4088628b023e0ae4496da6e08854390
Pavel Bakunovich <Pavel-Bakunovich@users.noreply.github.com>
2025-09-18 06:47:25 +0000

    Fixed a bug: date and day of week didn't update every day in system prompt

--- deepseek.py
+++ deepseek.py
@@ -7,14 +7,20 @@ import constants
 load_dotenv()
 DEEPSEEK_API_KEY = os.environ['DEEPSEEK_API_KEY']
 
-with open(constants.SYSTEM_PROMPT_FILENAME,"r") as system_prompt_file:
-    system_prompt = system_prompt_file.read()
-system_prompt = fill_template(system_prompt, day_of_week=get_day_of_week(), date=get_today_minsk_time_formatted())
+def get_refreshed_system_prompt():
+    with open(constants.SYSTEM_PROMPT_FILENAME,"r") as system_prompt_file:
+        system_prompt = system_prompt_file.read()
+    system_prompt = fill_template(system_prompt, day_of_week=get_day_of_week(), date=get_today_minsk_time_formatted())
+    return system_prompt
+
+system_prompt = get_refreshed_system_prompt()
 
 messages = [{"role": "system", "content": system_prompt}]
 
 def send_request(prompt, temperature):
     client = OpenAI(api_key=DEEPSEEK_API_KEY, base_url="https://api.deepseek.com")
+
+    messages[0]["content"] = get_refreshed_system_prompt()
 
     messages.append({"role": "user", "content": prompt})
     response = client.chat.completions.create(
@@ -27,3 +33,4 @@ def send_request(prompt, temperature):
     messages.append(response.choices[0].message)
 
     return response.choices[0].message.content
+
