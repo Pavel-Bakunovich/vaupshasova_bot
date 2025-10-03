2ca3a90ec34e537f8be2df305460e78a09a7ca16
Pavel Bakunovich <Pavel-Bakunovich@users.noreply.github.com>
2025-10-03 13:58:58 +0000

    Added option to view /my_stats on other players

--- command_my_stats.py
+++ command_my_stats.py
@@ -1,17 +1,18 @@
 from logger import log, log_error
 from telebot.types import ReactionTypeEmoji
 from helpers import get_next_matchday, get_next_matchday_formatted, get_today_minsk_time
-from common import add_player_if_not_existant, validate_access_no_game_registration_needed, text_to_image, get_player_name_formal, reply_only_CEO_can_do_it, validate_CEO_zone
+from common import add_player_if_not_existant_with_params, validate_access_no_game_registration_needed, text_to_image, get_player_name_formal, reply_only_CEO_can_do_it, validate_CEO_zone
 import database
 import constants
 import prettytable as pt
 
 def execute(message, bot):
     try:
-        current_player = add_player_if_not_existant(message.from_user.first_name,
-                                            message.from_user.last_name,
-                                            message.from_user.username,
-                                            message.from_user.id)
+        current_player = add_player_if_not_existant_with_params(message.text,
+                                                                message.from_user.first_name,
+                                                                message.from_user.last_name,
+                                                                message.from_user.username,
+                                                                message.from_user.id)
        
         if validate_access_no_game_registration_needed(message.chat.id, current_player, bot, message):
             season_stats_photo = season_stats(current_player)
